aea92c74d052f42598a415ba5d82c504af668bbd
taehyeonkweon <taehyeon.kweon@gmail.com>
2025-03-07 16:40:58 -0500

    Moved WAM URDF files to new directory

--- genesis/assets/urdf/wam_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/catkin_ws/src/ros-wam/wam_description
-# Build directory: /home/peter/catkin_ws/src/ros-wam/wam_description
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
